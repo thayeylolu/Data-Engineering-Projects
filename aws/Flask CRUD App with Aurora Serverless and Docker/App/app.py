8a0dd6b737d73f9e30fc591f9fa7c38cdb2d33a2
thayeylolu <hamdallahhassanah@gmail.com>
2023-03-28 18:52:40 -0700

    create schema from python code

--- aws/Flask CRUD App with Aurora Serverless and Docker/App/app.py
+++ aws/Flask CRUD App with Aurora Serverless and Docker/App/app.py
@@ -1,88 +1,79 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import os
-from dotenv import load_dotenv
-import boto3
-from flask import Flask, jsonify, request
-from botocore.config import Config
 
-# Load the values from the .env file into the environment variables
-load_dotenv()
+# initialize flask app
 app = Flask(__name__)
 
-config = Config(region_name=os.getenv('AWS_REGION'))
-rds_data = boto3.client(
-    'rds-data',
-    config=config,
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
-
-aurora_db_name = os.getenv('AURORA_DB_NAME')
-aurora_cluster_arn = os.getenv('AURORA_CLUSTER_ARN')
-aurora_secret_arn = os.getenv('AURORA_SECRET_ARN')
+# get database url
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
 
 
-def create_usr_acct_tbl():
-    create_table_command = '''CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        user_email VARCHAR(50) UNIQUE NOT NULL,
-        age INTEGER NOT NULL,
-        date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        date_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    );'''
-    return create_table_command
+# User Schema Class
+class User(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    # date_created = ...
 
 
-@app.route('/getUser')
-def getUser():
-    id = request.args.get('id')
-    response = callDbWithStatement("SELECT * FROM users WHERE id ='" + str(id) + "'")
-    user = {}
-    records = response['records']
-    for record in records:
-        user['id'] = record[0]['longValue']
-        user['username'] = record[1]['stringValue']
-        user['user_email'] = record[2]['stringValue']
-        user['age'] = record[3]['longValue']
-        user['date_created'] = record[4]['stringValue']
-    print(user)
-    return jsonify(user)
+# initialize class objects
+def __init__(self, username, email, age):
+    self.username = username
+    self.email = email
+    self.age = age
 
 
-@app.route('/AllUser')
-def getAllUser():
-    response = callDbWithStatement("SELECT * FROM users")
-    recorder = {}
-
-    all_records = response['records']  # 2
-    for user_info in range(len(all_records)):
-        records = all_records[user_info]
-        user = {'id': records[0]['longValue'], 'username': records[1]['stringValue'],
-                'user_email': records[2]['stringValue'], 'age': records[3]['longValue'],
-                'date_created': records[4]['stringValue']}
-        recorder[int(user['id'])] = user
-
-    print('*' * 30)
-    print(recorder)
-    print('*' * 30)
-    return jsonify(recorder)
+# create DB
+db.create_all()
 
 
-# @app.route('/createUser', methods = ['POST']):
+# get user by id endpoint
+@app.route('/users/<userid>', methods=['GET'])
+def get_user(userid):
+    user = User.query.get(userid)
+    del user.__dict__['_sa_instance_state']
+    return jsonify(user.__dict__)
 
 
-def callDbWithStatement(sql):
-    response = rds_data.execute_statement(
-        database=aurora_db_name,
-        resourceArn=aurora_cluster_arn,
-        secretArn=aurora_secret_arn,
-        sql=sql,
-        includeResultMetadata=True
-    )
-    print(f'Making Call... {sql}')
-    print('.-' * 20)
-    print(response)
-    return response
+# get all users endpoint
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = []
+    for user in db.session.query(User).all():
+        del user.__dict__['_sa_instance_state']
+        users.append(user.__dict__)
+    return jsonify(users)
+
+
+# create user endpoint
+@app.route('/users', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    db.session.add(User(body['username'], body['email'], body['age']))
+    db.session.commit()
+    return "user created"
+
+
+# update user endpoint
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(userid):
+    body = request.get_json()
+    db.session.query(User).filter_by(id=userid).update(
+        dict(email=body['email'], username=body['username'], age=body['age']))
+    db.session.commit()
+    return "User Details updated"
+
+
+# delete user by email
+@app.route('/users/<email>', methods=['DELETE'])
+def delete_item(email):
+    db.session.query(User).filter_by(id=email).delete()
+    db.session.commit()
+    return "User profile deleted"
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, host='0.0.0.0', port=8081)
+    app.run(debug=True)
