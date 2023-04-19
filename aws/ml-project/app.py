bf99453a9d2b44ee37fed79feb4420b9adf2c64a
thayeylolu <hamdallahhassanah@gmail.com>
2023-04-18 19:35:03 -0700

    add predic tapi

--- aws/ml-project/app.py
+++ aws/ml-project/app.py
@@ -6,9 +6,7 @@ import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
-## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -16,4 +14,26 @@ def home():
 if __name__=="__main__":
     app.run(debug=True)
 
+
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+   pass
+
+if __name__=="__main__":
+    app.run(debug=True)
 
