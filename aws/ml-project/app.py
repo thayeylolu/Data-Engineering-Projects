bf600292c146004862ff3efa8ad92ef619022f03
thayeylolu <hamdallahhassanah@gmail.com>
2023-05-15 23:48:06 -0700

    import libraries

--- aws/ml-project/app.py
+++ aws/ml-project/app.py
@@ -4,6 +4,8 @@ import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
+from scikit-learn import RidgeRegression
+import xgboost as xgb
 
 app=Flask(__name__)
 
